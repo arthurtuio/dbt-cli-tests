e063dee072b4a739b40b9fea93a75b55f5f893c0
arthurtuio <arthur.henrique.2709@gmail.com>
2021-05-27 15:19:44 -0300

    adding more first dbt proj files

---
+++ projeto-inicial-tuio/models/staging/stripe/stg_payments.sql
@@ -0,0 +1,7 @@
+select
+    id as payment_id,
+    order_id as order_id,
+    payment_method as payment_method,
+    -- amount is stored in cents, convert it to dollars
+    amount / 100 as amount
+from stripe.payment
