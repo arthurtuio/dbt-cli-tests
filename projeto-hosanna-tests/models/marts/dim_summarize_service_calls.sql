b304a518c5a4f8098b0dcbd4dfc6098755ee4692
arthurtuio <arthur.henrique.2709@gmail.com>
2021-06-03 15:57:23 -0300

    solving some errors caused by bad jinja use

--- projeto-hosanna-tests/models/marts/dim_summarize_service_calls.sql
+++ projeto-hosanna-tests/models/marts/dim_summarize_service_calls.sql
@@ -2,10 +2,10 @@
 -- {{ config(materialized='view', bind=False, schema='test_schema') }}
 
 SELECT
-    coalesce( email, 'abandon') AS email,
+    coalesce(email, 'abandon') AS email,
     round( sum(abandon_calls) / sum(received_calls) * 100, 2) as abandon_rate_phone,
     sum(received_calls) AS received_calls,
     sum(abandon_calls) AS abandon_calls,
     sum(tempo_falado) as total_duration_seconds
-FROM dev.spectrum_test_schema.stg_base_de_tma_e_qtde_de_ligacoes
+-- FROM dev.spectrum_test_schema.stg_base_de_tma_e_qtde_de_ligacoes
 FROM {{ ref('stg_base_de_tma_e_qtde_de_ligacoes') }}
--- projeto-hosanna-tests/models/staging/stg_base_de_tma_e_qtde_de_ligacoes.sql
+++ projeto-hosanna-tests/models/staging/stg_base_de_tma_e_qtde_de_ligacoes.sql
@@ -7,11 +7,11 @@ SELECT
     COALESCE(COUNT(*),0) AS "received_calls",
     COALESCE(COUNT(CASE WHEN (h.is_queue_status = 3 or h.is_queue_status = 4) THEN h.is_cdr_uniqueid ELSE NULL END ),0) AS "abandon_calls"
 FROM
-    spectrum.easy_core_contatos_ctrl as c
-    -- source('spectrum', 'easy_core_contatos_ctrl') as c
+    -- spectrum.easy_core_contatos_ctrl as c
+    {{ source('spectrum', 'easy_core_contatos_ctrl') }} as c
 JOIN
-    spectrum.easy_core_contatos_hist as h
-    -- source('spectrum', 'easy_core_contatos_hist') as h
+    --spectrum.easy_core_contatos_hist as h
+    {{ source('spectrum', 'easy_core_contatos_hist') }} as h
 ON
     h.easy_core_contatos_ctrl_id = c.id
 GROUP BY 1, 2
